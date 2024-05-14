bdfd468bc16b31331149ff9bbe1562de929070e3
Vijay-mehta <infovijay1998@gmail.com>
2024-05-14 06:50:31 +0530

    wishlist icon add

--- app/Ui/sidebar/book/BookCard.js
+++ app/Ui/sidebar/book/BookCard.js
@@ -2,14 +2,16 @@ import {
   CheckCircleIcon,
   XCircleIcon,
   CurrencyRupeeIcon,
+  HeartIcon,
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import AddToCart from "./AddToCart";
 
 const BookCard = ({ book }) => {
-
   return (
     <div className=" flex  flex-col  text-black   items-center  p-3">
+                <div className=" flex "><HeartIcon  className=" w-10  h-10   mt-2"/></div> 
+
       <h className=" text-black mb-2">{book.title}</h>
       <div className=" flex justify-center">
         <Image
@@ -19,12 +21,13 @@ const BookCard = ({ book }) => {
           className="rounded-md  h-[250px]"
         />
       </div>
+
       <div className="flex mt-2">
         {book.stock ? (
           <>
             <div className="flex">
               <CheckCircleIcon className="h-5 w-5 text-green-500" />
-              <p >{book.stock} in stock</p>
+              <p>{book.stock} in stock</p>
             </div>
           </>
         ) : (
@@ -35,12 +38,12 @@ const BookCard = ({ book }) => {
         )}
 
         <div className=" flex ">
-          <CurrencyRupeeIcon className="h-5 w-5"/>
+          <CurrencyRupeeIcon className="h-5 w-5" />
           <p>{book.sellPrice} to buy</p>
         </div>
       </div>
       <p className=" my-2">Rent for {book.rentPrice} INR per month </p>
-      <AddToCart book={book}/>
+      <AddToCart book={book} />
     </div>
   );
 };
--- app/Ui/sidebar/book/BookList.js
+++ app/Ui/sidebar/book/BookList.js
@@ -1,15 +1,22 @@
-const { default: BookCard } = require("./BookCard")
+const { default: BookCard } = require("./BookCard");
+import {
+    CheckCircleIcon,
+    XCircleIcon,
+    CurrencyRupeeIcon,
+    HeartIcon,
+  } from "@heroicons/react/24/solid";
+const BookList = ({ book }) => {
+  return (
+    <div className=" grid  grid-cols-1 lg:grid-cols-4 gap-4  w-full  text-black">
+      {book.map((book) => (
+        <div className=" shadow-sm  bg-white  flex justify-center  rounded-md">
+          {" "}
 
-const BookList=({book})=>{
-    return(
-        <div className=" grid  grid-cols-1 lg:grid-cols-4 gap-4  w-full ">
-        {book.map((book)=>(
-            <div className=" shadow-sm  bg-white  flex justify-center m-3  p-2  rounded-md">            <BookCard key={book.id} book={book}/>
-</div>
-        ))}
-        
+          <BookCard key={book.id} book={book} />
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default BookList;
