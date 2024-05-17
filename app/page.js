1d0b912898a7eeb9363cd2b6db56ed178a60bd8d
vijay mehta <76986956+Vijay-mehta@users.noreply.github.com>
2024-05-17 15:34:07 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
 import Image from "next/image";
-// import bgImage from "../../litloop/public/home/bg-image.jpg";
 import Link from "next/link";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 export default function Home() {
   return (
     <div className=" flex justify-center items-center h-screen">
-      // <Image 
-      // src="/home/bg-image.jpg" 
-      // alt="home bg-image"   
-      //   quality={100}
-      //   fill
-      //   sizes="100vw"
-      //   style={{
-      //     objectFit: "cover",
-      //   }} 
-      //   />
+      <Image 
+      src="/home/bg-image.jpg" 
+      alt="home bg-image"   
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }} 
+        />
       <div className=" z-10  bg-purple-100  py-16 px-2  rounded-md  ">
         <div className=" flex">
           <SparklesIcon width={50} height={50} className="text-purple-900" />
