fded3f830eb961a08c2efc6b5d965359b97dfa32
Vijay-mehta <infovijay1998@gmail.com>
2024-05-21 07:15:45 +0530

    set order detail page

--- app/Ui/sidebar/cart/OrderDetails.js
+++ app/Ui/sidebar/cart/OrderDetails.js
@@ -1,21 +1,44 @@
 "use client";
 import { storeContext } from "@/app/context";
-import { useContext, useEffect } from "react";
-import ProductCardItem from "./ProductCardItem";
+import { useContext, useEffect, useState } from "react";
 
 const OrderDetails = () => {
   const { cartData } = useContext(storeContext);
-  
+  const [priceToBuy, setPriceToBuy] = useState(0);
+  const [priceToRent, setPriceToRent] = useState(0);
 
+  useEffect(() => {
+    const buyTobook = cartData.filter((book) => {
+      return book.type === "buy";
+    });
+
+    const rentTobook = cartData.filter((book) => {
+      return book.type === "rent";
+    });
+
+    setPriceToBuy(buyTobook.reduce((pre, curr) => pre + curr.sellPrice, 0));
+
+    setPriceToRent(rentTobook.reduce((pre, curr) => pre + curr.rentPrice, 0));
+  }, [cartData]);
 
   return (
     <>
       {cartData.length > 0 && (
-        <div >
-          <div className=" bg-white   text-black  " >
-           <ProductCardItem cardItemData ={cartData} />
+        <div>
+          <h1 className=" font-bold mb-3">Welcome Mr.X</h1>
+          <h3 className=" font-medium  mb-4">
+            Thanku a lot for your purchase. Please complete the checkout process
+            by paying for your order{" "}
+          </h3>
+          <div className="mb-2   shadow-md  bg-white p-5 rounded-md">
+            <h1 className=" font-bold  mb-5">Order Details</h1>
+            <ul className="font-semibold">
+              <li className="font-normal">Buying book for {priceToBuy} NIR</li>
+              <li className="font-normal">
+                Renting book for {priceToRent} NIR
+              </li>
+            </ul>
           </div>
-        
         </div>
       )}
     </>
--- app/store/cart/page.js
+++ app/store/cart/page.js
@@ -3,12 +3,8 @@ import Payment from "@/app/Ui/sidebar/cart/payment";
 
 const Cart = () => {
   return (
-    <div className=" text-center mt-4">
-      <h1 className=" font-bold mb-3">Welcome Mr.X</h1>
-      <p>
-        Thanku a lot for your purchase. Please complete the checkout process by
-        paying for your order{" "}
-      </p>
+    <div className=" mt-7 ml-7">
+     
       <OrderDetails />
       <Payment />
     </div>
