32e8088cc16a786b16e7e0f04db630d1ad8b5e0c
Vijay-mehta <infovijay1998@gmail.com>
2024-05-25 19:18:25 +0530

    delete item form cart

--- app/Ui/sidebar/cart/CardItem.js
+++ app/Ui/sidebar/cart/CardItem.js
@@ -1,17 +1,30 @@
 import { TrashIcon } from "@heroicons/react/24/solid";
 
-const CardItem =({book})=>{
-    return(
-        <div className=" bg-gray-200 text-black  m-5  p-6  rounded-md flex">
-           
-       <div> <p className="bg-black text-white rounded-full  text-center mr-2 h-[25px] w-[30px]">{book.type ==="buy"? "B":"R" }</p></div>
-      <div>  <p className=" mb-3  font-bold">{book.title}</p>
-        <p className=" mb-3 font-light">{book.type === "buy" && `Buying book for ${ book.sellPrice} NIR`}</p>
-        <p className=" mb-3 font-light">{book.type === "rent" && `Renting book for ${book.rentPrice} NIR`}</p>
-        </div>
-       <div><TrashIcon className="h-6"/></div> 
-        </div>
-    )
-}
+const CardItem = ({ book,handleRemoveItem }) => {
 
-export default  CardItem;
+  return (
+    <div className=" bg-gray-200 text-black  m-5  p-6  rounded-md flex">
+      <div>
+        {" "}
+        <p className="bg-black text-white rounded-full  text-center mr-2 h-[25px] w-[30px]">
+          {book.type === "buy" ? "B" : "R"}
+        </p>
+      </div>
+      <div>
+        {" "}
+        <p className=" mb-3  font-bold">{book.title}</p>
+        <p className=" mb-3 font-light">
+          {book.type === "buy" && `Buying book for ${book.sellPrice} NIR`}
+        </p>
+        <p className=" mb-3 font-light">
+          {book.type === "rent" && `Renting book for ${book.rentPrice} NIR`}
+        </p>
+      </div>
+      <div onClick={(e) => handleRemoveItem(e, book.id,book.title)}>
+        <TrashIcon className="h-6" />
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
--- app/Ui/sidebar/cart/Items.js
+++ app/Ui/sidebar/cart/Items.js
@@ -1,19 +1,28 @@
-'use client'
+"use client";
 import { storeContext } from "@/app/context";
 import { useContext } from "react";
 import CardItem from "./CardItem";
+import { toast } from "react-toastify";
 
-const Items=()=>{
-    const { cartData } = useContext(storeContext);
-    return(
-        <div className="  bg-white ">
-            <h1 className="  font-bold  ml-5 mt-3">Items in Cart</h1>
-        {cartData.map((item)=>
-          <CardItem book={item}/>
-      
-        )}
-        </div>
-    )
-}
+const Items = () => {
+    const { cartData, setCartData } = useContext(storeContext);
+    console.log("cartData",cartData)
+  const handleRemoveItem = (e, id,title) => {
+    const filterData = cartData?.filter((item) => item.id !==id);
+    setCartData([...filterData ]);
+    toast.success(`remove successful ${title}`)
+  };
+  return (
+    <>
+    {cartData.length>0 && <><div className="  bg-white ">
+      <h1 className="  font-bold  ml-5 mt-3">Items in Cart</h1>
+      {cartData.map((item) => (
+       <div key={item.id}> <CardItem book={item} handleRemoveItem={handleRemoveItem} /></div>
+      ))}
+    </div></>}
+    </>
+    
+  );
+};
 
 export default Items;
