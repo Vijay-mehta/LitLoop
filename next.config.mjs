fcafd652a4ed5109cd2c1e9f03344a0b5f58cb29
Vijay-mehta <infovijay1998@gmail.com>
2024-05-28 08:29:06 +0530

    add config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      staticPageGenerationTimeout: 60, // Increase the timeout to 60 seconds
+    },
+  };
+  
+  module.exports = nextConfig;
+  
