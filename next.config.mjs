cf4c36653723a214436138f6b140f94513e855f5
Vijay-mehta <infovijay1998@gmail.com>
2024-05-28 09:05:54 +0530

    remove next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      staticPageGenerationTimeout: 60, // Increase the timeout to 60 seconds
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
