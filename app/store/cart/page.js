b136fa372288b02fef5c0734a7997e63bce44f73
Vijay-mehta <infovijay1998@gmail.com>
2024-05-28 21:26:45 +0530

    remove next config

--- app/store/cart/page.js
+++ app/store/cart/page.js
@@ -6,13 +6,13 @@ import { storeContext } from "@/app/context";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
 import { useContext } from "react";
 import React from "react";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "@/app/Ui/CheckoutForm";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+// import CheckoutForm from "@/app/Ui/CheckoutForm";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-);
+// const stripePromise = loadStripe(
+//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+// );
 
 const Cart = () => {
   const { cartData } = useContext(storeContext);
@@ -31,9 +31,9 @@ const Cart = () => {
               <Items />
               <OrderDetails />
               {/* <Payment /> */}
-              <Elements stripe={stripePromise}>
+              {/* <Elements stripe={stripePromise}>
                 <CheckoutForm />
-              </Elements>
+              </Elements> */}
             </div>
           </div>
         </>
