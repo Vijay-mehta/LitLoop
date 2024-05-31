98efc1e24ab0c7a14863df44ac97eb427a4e9e9d
Vijay-mehta <infovijay1998@gmail.com>
2024-05-31 14:31:35 +0530

    text color

--- app/store/about/page.js
+++ app/store/about/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <div className="max-w-3xl mx-auto p-6">
+    <div className="max-w-3xl mx-auto p-6  text-white">
       <section className="text-center mb-12">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">About Us</h1>
         <p className="text-lg text-gray-600">
