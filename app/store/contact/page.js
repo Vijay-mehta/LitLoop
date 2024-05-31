0255d07dc89664fab584331786050c3cbc1289e1
Vijay-mehta <infovijay1998@gmail.com>
2024-05-31 16:32:37 +0530

    iso bug fixe

--- app/store/contact/page.js
+++ app/store/contact/page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="max-w-3xl mx-auto p-6">
+    <div className="max-w-3xl mx-auto p-6  bg-white">
       <section className="text-center mb-12">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">Contact Us</h1>
         <p className="text-lg text-gray-600">
