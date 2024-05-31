23f35a535d63ac573bcdbaa9ec9aaa118e71bc33
Vijay-mehta <infovijay1998@gmail.com>
2024-05-31 23:01:24 +0530

    after success

--- app/success/page.js
+++ app/success/page.js
@@ -1,8 +1,18 @@
+'use client'
 import {
 
     CheckCircleIcon
   } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+
+
 const Success=()=>{
+
+    const route= useRouter();
+    setTimeout(()=>{ route.push('/store')},6000)
+      
     return(
         <div className="flex justify-center  text-black">
         <div className=" bg-white shadow-lg pX-24 py-24 mt-20 w-72  rounded-md  ">
